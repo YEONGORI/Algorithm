9fff937cc7a976c6df95af0ba5498d47e21df6ee
YEONGORI <dusrjf14@gmail.com>
2022-05-30 10:21:06 +0900

    done deque

--- deque/5430.cpp
+++ deque/5430.cpp
@@ -9,18 +9,30 @@ int main(void)
     cin.tie(0);
     cout.tie(0);
 
-    string funcs, str;
-    deque<int> DQ;
-    int t, n, R;
+    string func, str;
+    int t, n, R, tmp, err;
     cin >> t;
     for (int i = 0; i < t; i++)
     {
+        deque<int> DQ;
         R = 0;
-        cin >> funcs >> n >> str;
-        for (auto j : str)
-            if (j >= '0' && j <= '9')
-                DQ.push_back(j);
-        for (auto j : funcs)
+        err = 0;
+        cin >> func >> n >> str;
+
+        /*      문자열에서 정수를 뽑아 deque에 push     */
+        for (int i = 0; str[0] == '[' && i < (int)str.length(); i++)
+        {
+            tmp = 0;
+            if (str[i] >= '0' && str[i] <= '9')
+            {
+                while (str[i] != ',' && str[i] != ']')
+                    tmp = tmp * 10 + (str[i++] - '0');
+                DQ.push_back(tmp);
+            }
+        }
+
+        /*      함수들 실행     */
+        for (auto j : func)
         {
             if (j == 'R')
                 R += 1;
@@ -29,13 +41,41 @@ int main(void)
                 if (DQ.empty())
                 {
                     cout << "error\n";
+                    err = 1;
                     break;
                 }
-                else if (R / 2 == 0)
+                else if (R % 2 == 0)
                     DQ.pop_front();
                 else
                     DQ.pop_back();
             }
+        }
+
+        /*      deque에 남은 element들 출력     */
+        if (!err)
+        {
+            cout << '[';
+            if (R % 2 == 0)
+            {
+                while (!DQ.empty())
+                {
+                    cout << DQ.front();
+                    if (DQ.size() != 1)
+                        cout << ',';
+                    DQ.pop_front();
+                }
+            }
+            else
+            {
+                while (!DQ.empty())
+                {
+                    cout << DQ.back();
+                    if (DQ.size() != 1)
+                        cout << ',';
+                    DQ.pop_back();
+                }
+            }
+            cout << "]\n";
         }
     }
 }
