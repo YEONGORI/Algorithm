d5433636d0cb0c70b6a3b0b48703eb2714913c2c
YEONGORI <dusrjf14@gmail.com>
2022-08-02 13:26:44 +0900

    1941_1 clear

--- backtracking/1941_1.cpp
+++ backtracking/1941_1.cpp
@@ -12,6 +12,7 @@ int dx[4] = {-1,1,0,0};
 int dy[4] = {0,0,-1,1};
 
 pair<int, int> room[7][7];
+int v_room[7][7];
 int res[8];
 int result;
 
@@ -19,35 +20,21 @@ int ch_adj(void) {
 	int idx = 1;
 	queue<pair<int, int> > Q;
 	int vis[7][7] = {};
-	int v_room[7][7] = {};
 
-	for (int i = 0; i < 25; i++) {
-		if (i == res[idx]) {
-			if (idx == 1) {
-				vis[i/5][i%5] = 1;
-				Q.push(make_pair(i*5, i%5));
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if ((i * 5 + j) != res[idx])
+				v_room[i][j] = 0;
+			else {
+				if (idx == 1)
+					Q.push(make_pair(i, j));
+				v_room[i][j] = 1;
+				idx++;
 			}
-			v_room[i/5][i%5] = 1;
-			idx++;
 		}
-		else
-			v_room[i/5][i%5] = 0;
-		// for (int j = 0; j < 5; j++) {
-		// 	if ((i * 5 + j) == res[idx]) {
-		// 		if (idx == 1) {
-		// 			vis[i][j] = 1;
-		// 			Q.push(make_pair(i, j));
-		// 		}
-		// 		v_room[i][j] = 1;
-		// 		idx++;
-		// 	}
-		// 	else {
-		// 		v_room[i][j] = 0;
-		// 	}
-		// }
 	}
 
-	int cnt = 1;
+	int cnt = 0;
 	while (!Q.empty()) {
 		int cur_x = Q.front().first;
 		int cur_y = Q.front().second;
@@ -55,11 +42,11 @@ int ch_adj(void) {
 		for (int d = 0; d < 4; d++) {
 			int nx = cur_x + dx[d];
 			int ny = cur_y + dy[d];
-			if (nx < 0 || nx > 4 || ny < 0 || ny > 4)	continue;
+			if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5)	continue;
 			if (v_room[nx][ny] == 0 || vis[nx][ny])	continue;
 			vis[nx][ny] = 1;
-			Q.push(make_pair(nx, ny));
 			cnt++;
+			Q.push(make_pair(nx, ny));
 		}
 	}
 	return (cnt);
@@ -70,21 +57,24 @@ int ch_dasom(void) {
 
 	for (int i = 1; i <= 7; i++)
 		cnt += room[res[i] / 5][res[i] % 5].second;
-	if (cnt >= 4)
-		return (ch_adj());
-	return (0);
+	if (cnt < 4)
+		return (0);
+	return (ch_adj());
 }
 
 void func(int k) {
 	if (k == 8) {
-		if (ch_dasom() == 7)
+		int n = ch_dasom();
+		if (n == 7)
 			result++;
 		return ;
 	}
-	for (int i = 0; i < 25; i++) {
-		if (room[i/5][i%5].X > res[k - 1]) {
-			res[k] = room[i/5][i%5].X;
-			func(k + 1);
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (room[i][j].X > res[k - 1]) {
+				res[k] = room[i][j].X;
+				func(k + 1);
+			}
 		}
 	}
 }
@@ -105,20 +95,7 @@ int main(void)
 			room[i][j].X = idx++;
 		}
 	}
+	memset(res, -1, 8);
 	func(1);
 	cout << result;
 }
-
-/*
-YYYYY
-SYSYS
-YYYYY
-YSYYS
-YYYYY
-
-00000
-10101 5 7 9
-00000
-01010 16 18
-00000
-*/
