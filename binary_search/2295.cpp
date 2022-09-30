3a7c8e930d093ec877fe8108219c42e767475d8f
yeongori <dusrjf14@gmail.com>
2022-09-30 10:29:09 +0900

    chore: 세 수의 합

--- binary_search/2295.cpp
+++ binary_search/2295.cpp
@@ -1,10 +1,13 @@
 // 세 수의 합
 #include <iostream>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int n;
+int U[1002];
+vector<int> two;
 
 int main(void)
 {
@@ -12,4 +15,23 @@ int main(void)
     cin.tie(0);
 
     cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> U[i];
+    for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
+            two.push_back(U[i] + U[j]);
+    sort(two.begin(), two.end());
+    sort(U, U + n);
+    for (int i = n - 1; i > 0; i--)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            if (binary_search(two.begin(), two.end(), U[i] - U[j]))
+            {
+                cout << U[i];
+                return (0);
+            }
+        }
+    }
+    return (0);
 }
