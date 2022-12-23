b6f30c1b00af0bbf8800518fcff8ff41e16a63fe
YEONGORI <dusrjf14@gmail.com>
2022-12-23 10:57:38 +0900

    feat: 전화번호부 목록 해쉬(unordered_map)풀이

---
+++ Programmers/HASH/전화번호부_목록.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_set>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    unordered_set<string> hash_map;
+    sort(phone_book.begin(), phone_book.end());
+    
+    hash_map.insert(phone_book[0]);
+    for (int i=1; i<phone_book.size(); i++) {
+        string str = "";
+        for (int j=0; j<phone_book[i].length(); j++) {
+            str += phone_book[i][j];
+            if (hash_map.find(str) != hash_map.end())
+                return false;
+        }
+        hash_map.insert(phone_book[i]);
+    }
+    return true;
+}
