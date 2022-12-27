54c18c87582ef39ddfe696e58a2c4f5b8c40bfbc
yeongori <dusrjf14@gmail.com>
2022-12-27 23:33:43 +0900

    feat: 두 큐 합 같게 만들기 카카오 인턴쉽 문제

---
+++ Programmers/QUEUE/두큐합같게만들기.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> queue1, vector<int> queue2)
+{
+    unsigned long long total = 0, t1 = 0, t2 = 0;
+    int cnt = -1, i1 = 0, i2 = 0;
+    int maxCnt = queue1.size() * 3;
+    for (int i = 0; i < queue1.size(); i++)
+    {
+        t1 += queue1[i];
+        t2 += queue2[i];
+    }
+    total = t1 + t2;
+    if (total & 1)
+        return -1;
+    while (cnt++ < maxCnt)
+    {
+        if (t1 > t2)
+        {
+            t1 -= queue1[i1];
+            t2 += queue1[i1];
+            queue2.push_back(queue1[i1++]);
+        }
+        else if (t1 < t2)
+        {
+            t2 -= queue2[i2];
+            t1 += queue2[i2];
+            queue1.push_back(queue2[i2++]);
+        }
+        else
+            return cnt;
+    }
+    return -1;
+}
