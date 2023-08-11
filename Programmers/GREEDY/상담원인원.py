c7199f461988244e62384f668638f06a4bbf18b4
yeongori <dusrjf14@gmail.com>
2023-08-11 12:52:28 +0900

    [feat] 프로그래머스 상담원 인원 완전탐색 풀이

--- Programmers/GREEDY/상담원인원.py
+++ Programmers/GREEDY/상담원인원.py
@@ -1,75 +1,39 @@
 from heapq import heappush, heappop
-from copy import copy
-
-
-def cal_spend_time(mentorList, waitList, k):
-    time = 0
-
-    for _type in range(k):  # 타입 별로 대기 시간 계산
-        endTime = []
-        for _start, _spend, t in waitList:  # start_time, spend_time, type
-            if t != _type + 1:
-                continue
-            if mentorList[_type] > 0:
-                heappush(endTime, _start + _spend)
-                mentorList[_type] -= 1
-            else:
-                if _start < endTime[0]:
-                    time += endTime[0] - _start
-                heappush(endTime, endTime[0] + _spend)
-                heappop(endTime)
-    return time
-
-
-# 20, 40 = 60
-# 50, 45 = 95
-#          60 - 50
-
-# 5, 55 = 60
-# 10, 90 = 100
-# 100, 50 = 150
-#           100 - 60
+from itertools import combinations_with_replacement, permutations
 
 
 def solution(k, n, reqs):
-    mentors = [1] * k
-    dp = [1e9] * (n)
-    # for i in range(k):
-    #     dp[i] = 0
+    def cal_wait_time(waitings, n):  # 유형 별 waiting_list에 n명의 상담 원이 있을 때 대기 시간 계산
+        total_time = 0
+        counsel_list = []
+        for _ in range(n):
+            heappush(counsel_list, 0)
+        for start, duration in waitings:
+            prev_end = heappop(counsel_list)  # 자리가 생기는 시간
+            if start > prev_end:  # 바로 상담 가능
+                heappush(counsel_list, duration)
+            else:
+                wait_time = prev_end - start
+                total_time += wait_time
+                heappush(counsel_list, duration + wait_time)
+        return total_time
 
-    for i in range(k - 1, n):
-        if sum(mentors) < n:
-            idx = 0
-            for j in range(k):
-                mentors[j] += 1
-                time = cal_spend_time(copy(mentors), reqs, k)
-                if dp[i] > time:
-                    idx = j
-                    dp[i] = time
-                mentors[j] -= 1
-            mentors[idx] += 1
-        else:
-            dp[i] = cal_spend_time(copy(mentors), reqs, k)
-    return dp[n-1]
+    result = 1e9
 
+    waiting_list = [[] for _ in range(k)]
+    for req in reqs:
+        waiting_list[req[2]-1].append([req[0], req[0]+req[1]])
 
-print(solution(3, 5, [[10, 60, 1], [15, 100, 3], [20, 30, 1], [30, 50, 3], [50, 40, 1], [60, 30, 2], [65, 30, 1], [70, 100, 2]]
-               ))
-print(solution(2, 3, [[5, 55, 2], [10, 90, 2],
-      [20, 40, 2], [50, 45, 2], [100, 50, 2]]))
-print(solution(2, 3, [[5, 55, 2], [10, 90, 2], [
-    20, 40, 1], [50, 45, 1], [100, 50, 2]]))
-print(solution(1, 1, [[5, 55, 1], [55, 60, 1]]))
-print(solution(3, 3, [[10, 60, 1], [15, 100, 3], [20, 30, 1], [30, 50, 3], [50, 40, 1], [60, 30, 2], [65, 30, 1], [70, 100, 2]]
-               ))
+    cases = set()
+    for comb in combinations_with_replacement([i for i in range(1, n - k + 2)], r=k):
+        if sum(comb) == n:
+            for perm in permutations(comb, k):
+                cases.add(perm)
 
-70 
-  50
-100
-  50
-140
-  75
-115
-  85
-90
-  20
+    for case in cases:
+        tmp = 0
+        for i in range(k):
+            tmp += cal_wait_time(waiting_list[i], case[i])
+        result = min(result, tmp)
+
+    return result
